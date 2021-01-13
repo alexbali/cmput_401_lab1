c8d5e3d69313612719f1f7687e8a1c34fa2f15bd
alexbali <46386190+alexbali@users.noreply.github.com>
2021-01-13 11:43:32 -0700

    script being downloaded and printed

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import requests
 
 print(requests.__version__)
-
 print(requests.get('http://www.google.com/').status_code)
+
+script = requests.get('https://raw.githubusercontent.com/alexbali/cmput_401_lab1/master/test.py?token=APMEG62VD34RLHWVY3H4WZ27747PS')
+open('script_test.txt','w+').write(script.text)
+
+print(script.text)
