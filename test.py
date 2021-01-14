9a15eca9cdff2fbffce40e2c3325040e5ad2f349
alexbali <46386190+alexbali@users.noreply.github.com>
2021-01-13 17:31:16 -0700

    repo public now

--- test.py
+++ test.py
@@ -3,7 +3,5 @@ import requests
 print(requests.__version__)
 print(requests.get('http://www.google.com/').status_code)
 
-script = requests.get('https://raw.githubusercontent.com/alexbali/cmput_401_lab1/master/test.py?token=APMEG62VD34RLHWVY3H4WZ27747PS')
-open('script_test.txt','w+').write(script.text)
-
+script = requests.get('https://raw.githubusercontent.com/alexbali/cmput_401_lab1/master/test.py')
 print(script.text)
